5979d8f4654c0ca3c8e3e578a4cb4c259b834e8f
Alexander Torkolenko <87096878+torkolenko@users.noreply.github.com>
2021-08-03 13:03:26 +0300

    Update project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-    has_many :todos, dependent: :destroy
+    has_many :todos, -> { order('id DESC') }, dependent: :destroy
     # Указываю, что перед записью нужно проверять title на наличие и уникальность
     validates :title, presence: true, uniqueness: true
 end
